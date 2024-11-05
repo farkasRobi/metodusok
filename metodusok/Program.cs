3a314d6c4b1ba03e16215f3dc4fe1479a66f616e
farkasRobi <farkas.robert2007@gmail.com>
2024-11-05 09:32:16 +0100

    még metódusok

--- metodusok/Program.cs
+++ metodusok/Program.cs
@@ -6,7 +6,16 @@
 		{
 			Koszonto();
 			Console.WriteLine(Osszeadas(10, 90));
-			Console.ReadLine();
+			int a = 5;
+            Console.WriteLine(Duplaz(a));
+			int duplazott = Duplaz(a);
+            Console.WriteLine(a);
+            Console.WriteLine(duplazott);
+            Console.WriteLine(Osszeg(10));
+			int elso = 5;
+			int masodik = 10;
+			Csere(ref elso, ref masodik);
+            Console.WriteLine(elso + " " + masodik);
         }
 		static void Koszonto()
 		{
@@ -21,5 +30,24 @@
 			*/
 			return a + b;
 		}
+
+		static int Duplaz(int szam)
+		{
+			return szam = szam * 2;
+		}
+
+		static int Osszeg(int a, int b = 5, int c = 2)
+		{
+			return a + b + c;
+		}
+
+		static void Csere(ref int a, ref int b)
+		{
+			int temp = a;
+			a = b;
+			b=temp;
+            Console.WriteLine(a + "" + b);
+        }
+
 	}
 }
