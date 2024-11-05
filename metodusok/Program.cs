c3914cfa540ea696da097d5acd7de5f4d8a91c11
farkasRobi <farkas.robert2007@gmail.com>
2024-11-05 09:02:23 +0100

    kezdeti metódusok átnézése

--- metodusok/Program.cs
+++ metodusok/Program.cs
@@ -4,7 +4,22 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			Koszonto();
+			Console.WriteLine(Osszeadas(10, 90));
+			Console.ReadLine();
+        }
+		static void Koszonto()
+		{
+            Console.WriteLine("Hello világ!");
+        }
+
+		static int Osszeadas(int a, int b) 
+		{
+			/*
+			int osszeg = a + b;
+			return osszeg
+			*/
+			return a + b;
 		}
 	}
 }
